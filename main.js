2aa9b58f473ead9fa3b405a2f4f6da655663cd16
cinos81 <cinos81@gmail.com>
2023-09-08 11:01:04 +0900

    [FEAT] 기능 추가

--- main.js
+++ main.js
@@ -1,24 +1,57 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const currentSituation =  async (s, sd, ed) => {
+    try{
+        const currentAcademyNumber = document.getElementById("check-number02");
+        const academySumNumber = document.getElementById("academy-total-number");
+        const persentageCal = document.getElementById("persentage-cal");
+        const currentscenarioNumber = document.getElementById("check-number01");
+        const scenarioSumNumber = document.getElementById("scenario-total-number");
+        const scenarioCal = document.getElementById("scenario-cal");
+        const currentLibraryNumber = document.getElementById("check-number03");
+        const LibrarySumNumber = document.getElementById("library-total-number");
+        const LibraryCal = document.getElementById("library-cal");
+        const currentAssessmentNumber = document.getElementById("check-number04");
+        const AssessmentSumNumber = document.getElementById("assessment-total-number");
+        const AssessmentCal = document.getElementById("assessment-cal");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+        let result;
 
-setupCounter(document.querySelector('#counter'))
+        await fetch(`{{adminApiUrl}}/statistics/current?s=${s}&sd=${sd}&ed=${ed}`, {
+            method: "GET",
+            headers: getHeaders()
+        }).then((res) => res.json())
+            .then((data)=> {
+                console.log("현황 데이터", data);
+                currentAcademyNumber.textContent = data.academy.site_count
+                academySumNumber.textContent = data.academy.all_count
+                persentageCal.textContent = (`(${( (currentAcademyNumber.textContent / academySumNumber.textContent) * 100).toFixed(2)}%)`)
+
+
+
+
+
+                currentscenarioNumber.textContent = data.scenarios.site_count
+                scenarioSumNumber.textContent = data.scenarios.all_count
+                scenarioCal.textContent = (`(${( (currentscenarioNumber.textContent / scenarioSumNumber.textContent) * 100).toFixed(2)}%)`)
+
+
+                console.log(scenarioCal.textContent);
+
+                if( scenarioCal.textContent === "(NaN%)") {
+                    scenarioCal.textContent = "(100.00%)"
+                }
+
+
+                currentLibraryNumber.textContent = data.library.site_count
+                LibrarySumNumber.textContent = data.library.all_count
+                LibraryCal.textContent = (`(${( (currentLibraryNumber.textContent / LibrarySumNumber.textContent) * 100).toFixed(2)}%)`)
+                currentAssessmentNumber.textContent = data.assessment.site_count
+                AssessmentSumNumber.textContent = data.assessment.all_count
+                AssessmentCal.textContent = (`(${( (currentAssessmentNumber.textContent / AssessmentSumNumber.textContent) * 100).toFixed(2)}%)`)
+
+
+                return data
+            })
+    }catch(error) {
+        console.error('API 요청 에러:', error);
+    };
+}
